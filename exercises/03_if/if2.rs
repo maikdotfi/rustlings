d0edb9d91919085287ff68b81ff7a45c045e7e92
maikdotfi <25245521+maikdotfi@users.noreply.github.comm>
2025-04-04 17:33:37 +0300

    03 done

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,13 +2,17 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
     } else {
-        1
+        "No thanks!"
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let food = "potato";
+    picky_eater(food);
 }
 
 // TODO: Read the tests to understand the desired behavior.
