1e80e0d999b55d671639c962cf2c8fa34fb67b9e
maikdotfi <25245521+maikdotfi@users.noreply.github.comm>
2025-05-14 21:20:10 +0300

    quiz2 n options done

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,13 @@
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        return None;
+    }
+    if hour_of_day > 21 {
+        return Some(0);
+    }
+    return Some(5);
 }
 
 fn main() {
@@ -18,8 +25,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
-
+        let icecreams = maybe_icecream(12).unwrap();
         assert_eq!(icecreams, 5); // Don't change this line.
     }
 
--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -8,8 +8,10 @@ fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
+    // we can also put & here to use a reference in the switch
     match optional_point {
-        Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        // using ref here is valid, but & is not, bit confusing?
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
 
